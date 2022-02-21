0c4aaf0b3e95e0cab5f60e8961f494b943ced85e
bajraroshan <bajraroshan@gmail.com>
2022-02-21 23:37:23 +1030

    Slider updated, Image Optimized

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import BannerImage from "../../assets/hero-img.png";
+import { Container, Carousel } from "react-bootstrap";
+import BannerImage from "../../assets/hero-img-1.png";
+import BannerImage2 from "../../assets/hero-img-2.png";
 
 function Banner() {
   return (
-    <div id="home" className="hero-img" style={{backgroundImage: `url(${BannerImage})`}}>
-      <Container>   
-          <div className="column-full hero-img-content">
-            <h2>Hand Carved <span>Tara</span></h2>
+    <div id="home" className="hero-img">
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BannerImage} alt="First slide" />
+          <Carousel.Caption className="hero-img-content">
+            <h2>
+              Hand Carved <span>Tara</span>
+            </h2>
             <Link to="/products">Browse More Products</Link>
-          </div>
-        
-        </Container>
-      </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={BannerImage2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption className="hero-img-content">
+            <h2>
+              Hand Carved <span>Ghau</span>
+            </h2>
+            <Link to="/products">Browse More Products</Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
