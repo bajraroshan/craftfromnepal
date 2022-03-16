2635563ada8ca60ee6a1d7964260d97478b2bb51
bajraroshan <bajraroshan@gmail.com>
2022-03-16 16:35:32 +1030

    Stripe Fix Heroku

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,15 +1,15 @@
-const db = require('./connection');
+const db = require('../config/connection');
 const { User, Product, Category } = require('../models');
 
 db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Statues' },
+    { name: 'Silver Ornament' },
+    { name: 'Ritual Items' },
+    { name: 'Thangka' },
+    { name: 'Ghau and Pendant' }
   ]);
 
   console.log('categories seeded');
@@ -18,111 +18,102 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Shakyamuni Buddha Statue',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+        'Shakyamuni is the Buddha of our time. He is  the glorious root teacher of all the diverse lineages that make up Buddhism as we know it. Born Siddhartha Gautama more than 2500 years ago, he was the crown prince of the Shakya kingdom located in the Himalayan foothills of what is now southern Nepal. He left luxurious palace life behind him, however, in pursuit of the truth of   the origin of suffering and the cessation of fear. His astounding courage, determination, wisdom and compassion led to his perfect enlightenment despite seemingly insurmountable obstacles. His teachings have transformed individuals   and nations throughout the world and still speak to us today.',
+      image: 'buddha.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 999.99,
+      quantity: 5,
+      isfeatured: true
     },
     {
-      name: 'Canned Coffee',
+      name: 'Guru Padmasambhava Statue',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+        'This statue is a beautiful representation of the beloved Guru Rinpoche. Expertly sculpted from copper alloy and fully gilded with 24 karat gold, the statue has been finely carved with detail images of dragon and other images and has hand face painted. This sculpture was individually handcrafted in Patan, Nepal by master artisans of the Shakya clan who are considered among the best in the world. These craftsmen are the modern heirs to a centuries-old tradition of creating sacred art for use in temples and monasteries. The fine metalworking techniques have been passed down from generation to generation since ancient times.',
+      image: 'guru.jpg',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 799.99,
+      quantity: 5,
+      isfeatured: true
     },
     {
-      name: 'Toilet Paper',
+      name: 'Turquoise, Coral, Lapis Om Pendant',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+        'This listing is for a hand crafted crushed lapis and turquoise pendant with an OM symbol and beautiful scrolling detail. This pendant measures approximately 1 inch and was handmade in Kathmandu Nepal. In Tibetan Buddhism, "OM" is the supreme and most sacred syllable, consisting in Sanskrit of three sounds (A, U, M) representing body, speech, mind and other various fundamental triads. OM is believed to be the spoken essence of the universe; it is often understood as symbolizing the true "empty" character of reality, as that truth has been communicated by various historical Buddhas. Due to the handmade nature of this item, each pendant may differ slightly. Please contact me with any further questions.',
+      image: 'om-pendant.jpg',
+      price: 9.99,
+      quantity: 100,
+      isfeatured: true
     },
     {
-      name: 'Handmade Soap',
+      name: 'Silver Ring',
       category: categories[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+        "This listing is for a handmade Tibetan healing ring with carved Om mani pad hum mantra.  There are four options to choose from.  Two hand crafted Buddha of compasion three metal rings, one silver plated Buddha of compassion mantra ring and one with yak bone dzi bead.  This ring is adjustable and handmade in Kathmandu Nepal. <br /> <br /> The script is the mantra of Avalokteshwara the Buddha of compasstion and it means 'hail to the jewel in the lotus'. Mani means jewel and pema means lotus. OM includes three letters which are AH, O and MA and it is also Om Ah and the last letter HUM together makes the word OM AH HUM. This word is a blessing word, because whenever we bless something whether we are blessing the earth or food or whatever Tibetan people say OM AH HUM. After that you visualize that it is purified. So this mantra is very powerful mantra. There is no Tibetan people who don't know OM MANI PADME HUM mantra. We say this mantra every time when you see a dead being, sick person or animal. We visualize that this mantra will help the being. That is why Tibetan people wear wrist malas with these designs. ",
+      image: 'silver-ring.jpg',
+      price: 9.99,
+      quantity: 50,
+      isfeatured: true
     },
     {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
+      name: 'Bhumpa',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+        'These two matching Tibetan Ritual Bhumpa Set is made of copper alloy with Fine Hand Carvings and Decorated with Coral and Turquoise Semiprecious Stones. The carving details are done very finely throughout. The vases are identical except for the sacred syllables and other symbols inscribed around their rims. These finely carved Bhumpas are hand crafted by the  master artisans of Patan, Nepal.',
+      image: 'bhumpa.jpg',
+      price: 149.99,
+      quantity: 25,
+      isfeatured: true
     },
     {
-      name: 'Tablet',
+      name: 'Bajra & Bell Set',
       category: categories[2]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+        'This beautiful quality Bajra & Bell set is made of bronze alloy with gold plated and with beautifully carved images. The carving details are done very finely. This finely carved rare Vajra & Bell set is crafted by the master artisans of Bouddha, Nepal.',
+      image: 'bajra-bell.jpg',
+      price: 99.99,
+      quantity: 30,
+      isfeatured: true
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Chenrezig Thangka',
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+        'On Chenrezig’s left side, at the level of his heart, is the gentle face of a wild deer; the deer’s skin is draped over his shoulder. The deerskin is an ancient symbol of renunciation, as wandering mendicants often took the skin of a deer as a meditation seat, and sometimes as a garment and blanket. This symbolic meaning applies, as Chenrezig, a Bodhisattva, has renounced the snares of worldly existence, but here it further symbolizes his boundless compassion for sentient beings.  According to legend, Chenrezig once took rebirth as a wild deer, but he had such heartfelt compassion for the hunter who chased him that he willingly laid down his life.',
+      image: 'chengrisi.jpg',
+      price: 399.99,
+      quantity: 5,
+      isfeatured: true
     },
     {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
+      name: 'Kalachakra Mandala Thanka',
+      category: categories[3]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
+        'Kalachakra is a Sanskrit word meaning Wheel of Time or Cycle of Time. It is a complete and elaborately detailed cosmology that explains the creation and structure of all. In this sacred explanation, the microcosm that is man is not different from the macrocosm that is the Universe. The two very complex “maps” provided by the Kalachakra – one outside us, the other inside us – include methods for applying the knowledge that leads to true freedom and lasting happiness.<br /><br />The Kalachakra tradition of Tantric Buddhism focuses on the concepts of time and the ever-present cycles that pervade all life, from the movements of the planets to the intake and outflow of the breath. These teachings provide powerful techniques for harnessing and developing the subtle energies within the body, ultimately enabling practitioners to emerge victorious from the battle with the delusions that are the source of all suffering.<br /><br />The mandala itself depicts a sacred residence that is the abode of hundreds of meditational deities. This residence is an intricately-detailed three-dimensional five-storied palace where every detail has multiple layers of symbolic meaning. Each element of the mandala, from each individual deity to every single adornment of the palace, refers simultaneously to an aspect of the universe (outer Kalachakra), an aspect of the body/mind (inner Kalachakra), and an aspect of the Kalachakra Tantric practice.<br /><br />The practitioner strives to visualize every detail of the palace and the deities within it to a level of perfect life-like realism and imagines walking through the building and encountering all who dwell within it. Through developing the power of meditative concentration to such a degree, the practitioner skillfully trains the mind and approaches the state of enlightenment.',
+      image: 'mandala-thanka.jpg',
+      price: 599.99,
+      quantity: 3,
+      isfeatured: true
     },
     {
-      name: 'Teddy Bear',
+      name: 'Aparmita Silver Ghau',
       category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
+      description: 'Ghau is a small prayer box worn as pendant by Buddhists as portable shrines where prayers and sacred relics are kept and used during their travel to hold their sacred deity inside. Now, Ghau or Prayer Box pendants are often used as jewelry or personal adornment. They are used to carry prayers or prayer concerns or photos, poems, small items, or memories. Ghau pendants can carry your prayers near to your heart throughout the day. They are jewelry that has a history. The Ghau opens to a concealed inner space, and is traditionally used by Tibetan Buddhists to hold a picture of their favorite deity or Lama, a folded up scroll of sacred mantras, special herbs or sacred relics. The Ghau is used as an amulet to help the wearer to ward off negative energy and attract blessings. A Ghau are also used as a portable shrine and are worn on a cord around the neck and hung close to the heart.',
+      image: 'ghau1.jpg',
+      price: 199.99,
+      quantity: 40,
+      isfeatured: false
     },
     {
-      name: 'Alphabet Blocks',
+      name: 'Tibeaten Ghau Box',
       category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+      description: 'Ghau is a small prayer box worn as pendant by Buddhists as portable shrines where prayers and sacred relics are kept and used during their travel to hold their sacred deity inside. Now, Ghau or Prayer Box pendants are often used as jewelry or personal adornment. They are used to carry prayers or prayer concerns or photos, poems, small items, or memories. Ghau pendants can carry your prayers near to your heart throughout the day. They are jewelry that has a history. The Ghau opens to a concealed inner space, and is traditionally used by Tibetan Buddhists to hold a picture of their favorite deity or Lama, a folded up scroll of sacred mantras, special herbs or sacred relics. The Ghau is used as an amulet to help the wearer to ward off negative energy and attract blessings. A Ghau are also used as a portable shrine and are worn on a cord around the neck and hung close to the heart.',
+      image: 'ghau2.jpg',
+      price: 149.99,
+      quantity: 1000,
+      isfeatured: false
     }
   ]);
 
@@ -131,21 +122,23 @@ db.once('open', async () => {
   await User.deleteMany();
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
+    firstName: 'Ersa',
+    lastName: 'Shakya Bajracharya',
+    email: 'ersa@email.com',
+    password: 'password12345'
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'Elsa',
+    lastName: 'Shakya',
+    email: 'elsa@email.com',
+    password: 'password12345'
+  });
+
+  await User.create({
+    firstName: 'Roshan',
+    lastName: 'Bajracharya',
+    email: 'roshan@email.com',
     password: 'password12345'
   });
 
